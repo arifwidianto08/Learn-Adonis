9b0ebac41308f2eacb8dfc8dba193f905bb680f8
Arif Widianto <arifjozz123@gmail.com>
2019-06-27 16:41:14 +0700

    unused all env

--- config/database.js
+++ config/database.js
@@ -27,18 +27,7 @@ module.exports = {
       port: 27017,
       username: 'expoabsensi',
       password: 'expoabsensi123',
-      database: 'test',
-      options: {
-        // replicaSet: Env.get('DB_REPLICA_SET', '')
-        // ssl: ('DB_SSL, '')
-        // connectTimeoutMS: Env.get('DB_CONNECT_TIMEOUT_MS', 15000),
-        // socketTimeoutMS: Env.get('DB_SOCKET_TIMEOUT_MS', 180000),
-        // w: ('DB_W, 0),
-        // readPreference: Env.get('DB_READ_PREFERENCE', 'secondary'),
-        // authSource: Env.get('DB_AUTH_SOURCE', ''),
-        // authMechanism: Env.get('DB_AUTH_MECHANISM', ''),
-        // other options
-      }
+      database: 'test'
     }
   }
 };
